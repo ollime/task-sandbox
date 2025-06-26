4930d80d5d72bb529c5db9b23afbff14558087e9
Jennie Long <longjen100@gmail.com>
2025-06-26 14:53:44 -0700

    added first component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'productive-app';
