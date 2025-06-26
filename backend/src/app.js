4666646cf94cfc37c6484c7fa01292687587492d
Jennie Long <longjen100@gmail.com>
2025-06-26 13:55:06 -0700

    added more backend files

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,27 @@
 import express from "express";
 import cors from "cors";
+import userRouter from "./routes/auth.route.js";
 
 const app = express();
 
 app.use(express.json());
+
+// app.use(cors({
+//     origin: function(origin, callback) {
+//         if (!origin) return callback(null, true);
+
+//         if(origin.startsWith("https://localhost:") || origin.startsWith("127.168.1.1") {
+//             return callback(null, true);
+//         })
+
+//         return callback(new Error("Not allowed by CORS"));
+//     },
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"]
+// }))
+
+// declare routes
+app.use("/api/v1/auth", userRouter);
 
 export default app;
--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,68 @@
-import { User } from "../models/users.model";
+import { User } from "../models/user.model.js";
 
 // implement generateAccessAndRefreshTokens
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = awaitUser.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating refresh and access token."
+    );
+  }
+};
+
+// registering a user
+
+const registerUser = async (req, res) => {
+  try {
+    const { username } = req.body;
+
+    if (!username) {
+      return res.status(400).json({ message: "All fields are required." });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(409).json({ message: "User already exists." });
+    }
+
+    const user = new User({ username });
+
+    await user.save();
+
+    try {
+      const accessToken = user.generateAccessToken();
+      const refreshToken = user.generateRefreshToken();
+
+      res.status(201).json({
+        message: "User registered successfully.",
+        accessToken,
+        refreshToken,
+        user: {
+          username: user.username,
+        },
+      });
+    } catch (tokenError) {
+      console.log("Token generation error:" + tokenError);
+      res.status(201).json({
+        message: "User registered successfully. (Token generation failed)",
+        user: {
+          username: user.username,
+        },
+      });
+    }
+  } catch (err) {
+    console.error("Register error: " + err);
+    res.status(500).json({ message: "Server error." });
+  }
+};
+
+export { registerUser };
