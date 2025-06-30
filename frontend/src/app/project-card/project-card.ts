33f4ba8fb003136d87ea9936fc920eefca3dd05f
Jennie Long <longjen100@gmail.com>
2025-06-29 22:53:43 -0700

    added progress bar component

--- frontend/src/app/project-card/project-card.ts
+++ frontend/src/app/project-card/project-card.ts
@@ -6,6 +6,7 @@ import {
   MatCardSubtitle,
   MatCardContent,
 } from '@angular/material/card';
+import { ProgressBar } from '../progress-bar/progress-bar';
 
 @Component({
   selector: 'app-project-card',
@@ -15,6 +16,7 @@ import {
     MatCardTitle,
     MatCardSubtitle,
     MatCardContent,
+    ProgressBar,
   ],
   templateUrl: './project-card.html',
   styleUrl: './project-card.css',
