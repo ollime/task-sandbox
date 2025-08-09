69fcaedb34dff93cd1750d5ac666675b4bd85ef0
Jennie Long <longjen100@gmail.com>
2025-08-09 03:20:47 -0700

    added client side routes for mongodb api

---
+++ frontend/src/app/api/cards/route.ts
@@ -0,0 +1,17 @@
+import clientPromise from "../../../lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const client = await clientPromise;
+    const db = client.db("database");
+    const cards = await db.collection("tasks").find({}).toArray();
+    return NextResponse.json(cards);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
