5c6d0b88d04d6736e9be8906d2e5f3800512af94
Jennie Long <olimegreendino@gmail.com>
2025-08-12 04:20:09 -0700

    updated footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,23 +4,33 @@ import BugReportIcon from '@mui/icons-material/BugReport'
 
 export default function Footer() {
   return (
-    <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://github.com/ollime/productive-app"
-        target="_blank"
-        rel="noopener noreferrer">
-        <CodeIcon />
-        Source code
-      </a>
-      <a
-        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-        href="https://github.com/ollime/productive-app/issues"
-        target="_blank"
-        rel="noopener noreferrer">
-        <BugReportIcon />
-        Report issues
-      </a>
+    <footer className="row-start-3 flex w-full flex-wrap items-center justify-between">
+      <div className="space-x-4">
+        <a
+          className="flex items-center gap-1 text-sm hover:underline hover:underline-offset-4"
+          href="https://github.com/ollime/productive-app"
+          target="_blank"
+          rel="noopener noreferrer">
+          <CodeIcon fontSize="small" />
+          Source
+        </a>
+        <a
+          className="flex items-center gap-1 text-sm hover:underline hover:underline-offset-4"
+          href="https://github.com/ollime/productive-app/issues"
+          target="_blank"
+          rel="noopener noreferrer">
+          <BugReportIcon fontSize="small" />
+          Report issues
+        </a>
+      </div>
+      <div className="space-x-4">
+        <button className="rounded-lg bg-white p-2 text-sm text-black">
+          Save position
+        </button>
+        <button className="rounded-lg bg-white p-2 text-sm text-black">
+          Lock position
+        </button>
+      </div>
     </footer>
   )
 }
