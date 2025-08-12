0c3c114b0ad136f86b36f2dce0b5dffcf6af2309
Jennie Long <olimegreendino@gmail.com>
2025-08-11 19:31:56 -0700

    added api route for updating & deleting card

---
+++ src/app/api/cards/[id]/route.ts
@@ -0,0 +1,80 @@
+import { Task } from '@/models/cards.model'
+import { connectToDatabase } from '@/lib/mongodb'
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  connectToDatabase()
+  try {
+    const taskFound = await Task.findById(params.id)
+
+    if (!taskFound)
+      return NextResponse.json(
+        {
+          message: 'Task not found',
+        },
+        {
+          status: 404,
+        }
+      )
+
+    return NextResponse.json(taskFound)
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 400 })
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await req.json()
+  connectToDatabase()
+
+  try {
+    const taskUpdated = await Task.findByIdAndUpdate(params.id, body, {
+      new: true,
+    })
+
+    if (!taskUpdated)
+      return NextResponse.json(
+        {
+          message: 'Task not found',
+        },
+        {
+          status: 404,
+        }
+      )
+
+    return NextResponse.json(taskUpdated)
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 400 })
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  connectToDatabase()
+
+  try {
+    const taskDeleted = await Task.findByIdAndDelete(params.id)
+
+    if (!taskDeleted)
+      return NextResponse.json(
+        {
+          message: 'Task not found',
+        },
+        {
+          status: 404,
+        }
+      )
+
+    return NextResponse.json(taskDeleted)
+  } catch (err) {
+    return NextResponse.json({ error: err }, { status: 400 })
+  }
+}
--- src/app/api/cards/route.ts
+++ src/app/api/cards/route.ts
@@ -1,32 +1,32 @@
-import { connectToDatabase } from "../../../lib/mongodb";
-import { NextRequest, NextResponse } from "next/server";
-import { Task } from "../../../models/cards.model";
+import { Task } from '@/models/cards.model'
+import { connectToDatabase } from '@/lib/mongodb'
+import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET() {
   try {
-    await connectToDatabase();
-    const cards = await Task.find({});
-    return NextResponse.json(cards);
+    await connectToDatabase()
+    const cards = await Task.find({})
+    return NextResponse.json(cards)
   } catch (err) {
-    console.error(err);
+    console.error(err)
     return NextResponse.json(
-      { error: "Internal Server Error" },
+      { error: 'Internal Server Error' },
       { status: 500 }
-    );
+    )
   }
 }
 
 export async function POST(req: NextRequest) {
   try {
-    const body = await req.json();
-    await connectToDatabase();
-    console.log(body);
+    const body = await req.json()
+    await connectToDatabase()
+    console.log(body)
 
-    const newTask = new Task(body);
-    const savedTask = await newTask.save();
-    return NextResponse.json(savedTask);
+    const newTask = new Task(body)
+    const savedTask = await newTask.save()
+    return NextResponse.json(savedTask)
   } catch (err) {
-    console.error(err);
-    return NextResponse.json({ error: "Bad Request" }, { status: 400 });
+    console.error(err)
+    return NextResponse.json({ error: 'Bad Request' }, { status: 400 })
   }
 }
