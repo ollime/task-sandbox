43a5847852e4c26673e0faaec6e44470e0ca4664
Jennie Long <olimegreendino@gmail.com>
2025-08-17 02:48:21 -0700

    added ability to edit label

--- src/components/label.tsx
+++ src/components/label.tsx
@@ -1,5 +1,42 @@
 import { LabelData } from '@/types/label.types'
+import { useState } from 'react'
 
 export default function Label({ label, position }: LabelData) {
-  return <p className="text-lg">{label}</p>
+  const [isEditable, setIsEditable] = useState<boolean>(false)
+  const [text, setText] = useState<string>(label ?? '')
+
+  function handleToggleEdit() {
+    setIsEditable(!isEditable)
+  }
+
+  function handleEditText(value: string) {
+    setText(value)
+  }
+
+  return (
+    <div>
+      {isEditable ? (
+        <input
+          type="text"
+          value={text}
+          onChange={(evt) => {
+            handleEditText(evt.target.value)
+          }}
+          autoFocus={true}
+          onKeyDown={(evt) => {
+            if (evt.key === 'Enter') {
+              evt.preventDefault()
+              setIsEditable(false)
+            }
+          }}
+        />
+      ) : (
+        <p
+          className="text-lg hover:cursor-pointer"
+          onDoubleClick={handleToggleEdit}>
+          {text}
+        </p>
+      )}
+    </div>
+  )
 }
