d3badd303c01db3f18ca1679edebc95c5672ba02
Jennie Long <olimegreendino@gmail.com>
2025-08-18 14:10:42 -0400

    added app name

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -45,21 +45,21 @@ export default function LoginPage() {
 
   return (
     <div>
-      <h1 className="p-4 text-center text-2xl">App name</h1>
+      <h1 className="p-4 text-center text-2xl">Taskbox</h1>
       <form
         onSubmit={handleSubmit}
         className="flex flex-col items-center space-y-4 p-2">
         <input
           type="username"
           name="username"
-          placeholder="testuser"
+          placeholder="username"
           className="w-60 p-2"
           required
         />
         <input
           type="password"
           name="password"
-          placeholder="testtest"
+          placeholder="password"
           className="w-60 p-2"
           required
         />
