17d98a820367564a982945e0f340393159554c47
Jennie Long <olimegreendino@gmail.com>
2025-08-20 08:56:46 -0400

    working on table display

--- src/app/archive/[userId]/page.tsx
+++ src/app/archive/[userId]/page.tsx
@@ -12,10 +12,11 @@ export default function ArchivePage({
   params: Promise<{ userId: string }>
 }) {
   const [data, setData] = useState<Array<CardData>>([
-    { label: 'djflsfd', size: 'lgRect', rotated: true },
-    { label: 'djflsfd', size: 'mdSquare', rotated: true },
+    { label: 'dskf', size: 'lgRect', rotated: true },
+    { label: 'eep', size: 'mdSquare', rotated: true },
     { label: 'djflsfd', size: 'lgSquare', rotated: true },
   ])
+  const [isCompact, setIsCompact] = useState<boolean>()
   const { userId } = use(params)
   const router = useRouter()
 
@@ -48,22 +49,46 @@ export default function ArchivePage({
 
   return (
     <Container>
-      <div className="flex flex-row items-center space-x-2">
-        <ArrowBack onClick={handleRedirectToGrid} />
-        <h1 className="text-xl">Archive</h1>
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-row space-x-2">
+          <ArrowBack onClick={handleRedirectToGrid} />
+          <h1 className="text-xl">Archive</h1>
+        </div>
+        <div
+          onClick={() => {
+            setIsCompact(!isCompact)
+          }}>
+          Display: {isCompact ? 'Compact' : 'Table'}
+        </div>
       </div>
-      <div className="flex flex-wrap space-y-4 space-x-4">
-        {data.map((item) => (
-          <ArchiveCard
-            key={item.label}
-            label={item.label}
-            color={item.color}
-            size={sizePreset[item.size as SizeKeys]}
-            cardId={item._id ?? item.label}
-            rotation={item.rotated}
-          />
-        ))}
-      </div>
+      {isCompact ? (
+        <div className="flex flex-wrap space-y-4 space-x-4">
+          {data.map((item) => (
+            <ArchiveCard
+              key={item.label}
+              label={item.label}
+              color={item.color}
+              size={sizePreset[item.size as SizeKeys]}
+              cardId={item._id ?? item.label}
+              rotation={item.rotated}
+            />
+          ))}
+        </div>
+      ) : (
+        <table className="flex flex-col space-y-4">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>itlsfjsldfem</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
     </Container>
   )
 }
